2fa47c682bf97304fb2889bdcecf5bd1031a843e
lucinamay <119406697+lucinamay@users.noreply.github.com>
2025-04-01 13:55:23 +0200

    NEW: pymupdf extraction

---
+++ pdf_extraction/pymupdf_test.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from tqdm import tqdm
+
+
+import pymupdf
+import pymupdf4llm
+import pandas as pd
+
+
+def pymupdf_to_text(filepath):
+    with pymupdf.open(filepath) as pdf_document:
+        try:
+            text = ""
+            for page in pdf_document:
+                text += page.get_text()
+            return text
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+
+
+def pymupdf4llm_to_md(filepath):
+    try:
+        return pymupdf4llm.to_markdown(filepath)
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+def table_extractor(filepath, *args, **kwargs) -> list:
+    try:
+        with pymupdf.open(filepath) as pdf_document:
+            tables = []
+            for page in pdf_document:
+                results = page.find_tables(*args, **kwargs)
+                if results.tables:
+                    tables.extend(results.tables)
+            return tables
+    except Exception as e:
+        print(f"Table extractor error: {e}")
+        return None
+
+
+def table_writer(tables, out_name):
+    for i, table in enumerate(tables):
+        df = table.to_pandas()
+        df.to_csv(f"{out_name}_{i}.csv", index=False)
+        md = table.to_markdown()
+        with open(f"{out_name}_{i}.md", "w") as f:
+            f.write(md)
+
+
+def try_all(documents, out_folder):
+    Path(out_folder).mkdir(exist_ok=True)
+    Path(f"{out_folder}/p_txt").mkdir(exist_ok=True)
+    Path(f"{out_folder}/p4_md").mkdir(exist_ok=True)
+    Path(f"{out_folder}/tables").mkdir(exist_ok=True)
+
+    for filepath in tqdm(
+        Path(documents).glob("*.pdf"), total=len(list(Path(documents).glob("*.pdf")))
+    ):
+        outputs = {
+            "p_txt": pymupdf_to_text(filepath),
+            "p4_md": pymupdf4llm_to_md(filepath),
+        }
+
+        for key, output in outputs.items():
+            if not output:
+                with open(f"{out_folder}/error.log", "a") as f:
+                    f.write(f"Error: {key} - {filepath}\n")
+                continue
+            with open(
+                f"{out_folder}/{key}/{filepath.stem}.{key.split('_')[-1]}", "w"
+            ) as f:
+                f.write(output)
+
+        # tables = table_extractor(filepath, strategy="lines_strict")
+        # if tables:
+        #     table_writer(tables, f"{out_folder}/tables/{filepath.stem}")
+
+
+def main():
+    input_folder = Path(__file__).parent.parent / "documents"
+    output_folder = Path(__file__).parent.parent / "outputs"
+    print(input_folder)
+    try_all(input_folder, output_folder)
+
+
+if __name__ == "__main__":
+    main()
